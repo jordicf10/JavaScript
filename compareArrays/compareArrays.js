8019e9d59d2c19ceb5a7f6e9a8e1432a5edb7e84
= <=>
2022-03-04 21:20:30 +0100

    Comparing two number arrays without comas

--- compareArrays/compareArrays.js
+++ compareArrays/compareArrays.js
@@ -8,22 +8,43 @@ for(i=0;i<courses.length;i++){
     
     if(courses[i]>arr[i]){
         
-        coursesAnswer[i] = courses[i] + ", located at the first array, is bigger than " + arr[i] + ", located at the second array. "; 
-        arrTotal.push(coursesAnswer[i]);  
+        coursesAnswer[i] = courses[i] + " is located at the first array. It's bigger than " + arr[i] + " which is located at the second array" + "<br>"; 
+        arrTotal.push(coursesAnswer[i] + "<br>");  
     }else if(courses[i]<arr[i]){
         
-        arrAnswer[i] = arr[i] + ", located at the second array, is bigger than " + courses[i] + ", located at the first array. "; 
-        arrTotal.push(arrAnswer[i]);
+        arrAnswer[i] = arr[i] + " is located at the second array. It's bigger than " + courses[i] + " which is located at the first array" + "<br>"; 
+        arrTotal.push(arrAnswer[i] + "<br>");
         
     }
 }
+
 return arrTotal;
 }
 
-var courses = [1,2,3,4,5,6,7,8];
-var arr = [8,7,6,5,4,3,2,1];
+var num = prompt("How many numbers do you want in first array?");
+var courses = [];
+courses.length = num;
+
+
+var num2 = prompt("How many numbers do you want in second array?");
+var arr = [];
+arr.length = num;
+
+for(i=0;i<courses.length;i++){
+    courses[i]= prompt("Introduce number nº " + (i+1)+ ":");
+    }
+
+
+for(i=0;i<arr.length;i++){
+    arr[i]= prompt("Introduce number nº " + (i+1) + ":");
+}
+
 var arrtodot = [];
-console.log(getLargerNumbers(courses,arr));
+var cadena = new String(getLargerNumbers(courses,arr));
+
+document.write(cadena.replace(/,/g,""));
+
+
     
 
 
